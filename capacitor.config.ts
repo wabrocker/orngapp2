8953cf63635339a0b501efb674fd7ec948ce1627
wabrocker <billbrocker@gmail.com>
2021-08-30 19:20:07 -0400

    initial comment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.billbrocker.orngapp2',
   appName: 'OrngApp2',
   webDir: 'www',
   bundledWebRuntime: false
